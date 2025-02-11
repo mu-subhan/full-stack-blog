6f3f8c07ad339ed3d1e30ebd9e87bc14605b5d9d
Muhammad Subhan <muhammadsubhan8934@gmail.com>
2025-02-11 22:24:57 +0500

    add mongodb connection

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,7 @@
 import express from "express"
+import dotenv from "dotenv";
+import connectDB from "./lib/connectDB.js";
+dotenv.config();
 
 const app = express()
 
@@ -6,9 +9,9 @@ app.get("/",(req,res)=>{
     res.status(200).send("its work")
 })
 
-
-console.log(process.env.test)
+// console.log(process.env.MONGO)
 app.listen(3000,()=>{
     console.log("server is running!");
+    connectDB();
     
 })
