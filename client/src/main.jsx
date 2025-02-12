516eed0c918e9ea690a6b7dfdfdd608c0015980a
Muhammad Subhan <muhammadsubhan8934@gmail.com>
2025-02-12 14:08:13 +0500

    fatching data

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,9 +7,13 @@ import Write from './routes/Write.jsx'
 import PostListPage from './routes/PostListPage.jsx'
 import HomePage from './routes/HomePage.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import MainLayout from './layouts/MainLayout.jsx'
+import MainLayout from './layouts/MainLayout'
 import { ClerkProvider } from '@clerk/clerk-react'
-import SinglePostPage from './routes/SinglePostPage.jsx'
+import SinglePostPage from './routes/SinglePostPage'
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
@@ -50,12 +54,15 @@ const router = createBrowserRouter([
 ],
 
 )
-
+const queryClient = new QueryClient();
 createRoot(document.getElementById('root')).render(
   
   <StrictMode>
   <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+    <QueryClientProvider client={queryClient}>
    <RouterProvider router={router}/>
+
+    </QueryClientProvider>
    </ClerkProvider>
   </StrictMode>,
 )
