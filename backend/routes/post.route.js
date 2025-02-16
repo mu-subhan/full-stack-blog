1a09e6c8fcb6f7dc735537fbc592b461856620aa
Muhammad Subhan <muhammadsubhan8934@gmail.com>
2025-02-17 01:03:01 +0500

    complete project

--- backend/routes/post.route.js
+++ backend/routes/post.route.js
@@ -1,13 +1,14 @@
 import express from "express"
 import { createPost, getPost, getPosts,deletePost,uploadAuth,featurePost } from "../controllers/post.controller.js";
 import { requireAuth } from "@clerk/express";
+import increaseVisit from "../middlewares/increaseVisit.js";
 
 const router = express.Router();
 
 
 router.get("/upload-auth",uploadAuth)
 router.get("/",getPosts)
-router.get("/:slug",getPost)
+router.get("/:slug",increaseVisit,getPost)
 router.post("/",requireAuth(),createPost)
 router.delete("/:id",deletePost)
 router.patch("/feature",featurePost)
